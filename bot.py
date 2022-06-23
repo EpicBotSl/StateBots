8794c40aeffd9ec18e6d3ed44bf5e2981d6d9fea
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-23 10:45:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -88,7 +88,7 @@ async def back_menu(_,query):
 @app.on_callback_query(filters.regex("BOT_CALLBACK"))
 async def help_menu(_,query):
   await query.answer()
-  await query.message.edit(BOT_LIST_MG,reply_markup=REPLY_BUTTONS)
+  await query.message.edit(BOT_LIST_MG, reply_markup=REPLY_BUTTONS)
   
 print("EPIC BOTS🇱🇰")
 app.run()
